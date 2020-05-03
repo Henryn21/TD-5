a5221f76111f03168fcfd3387d3e0c20bfb9f86e
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2020-05-02 21:40:26 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
+//AJAX API
 let xhr= new XMLHttpRequest();
 //'https://randomuser.me/api/'
-xhr.onreadystatechange= function(){
+xhr.onreadystatechange= function(){//when state of xhr changes
     if(this.readyState===4 && this.status === 200) {
         console.log("working");
         let data=JSON.parse(xhr.responseText);
-        
-        
         console.log(data);
+        console.log(data.results.gender);
     }
     else{
         console.log("not working");
