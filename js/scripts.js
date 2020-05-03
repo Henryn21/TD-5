edc15beb9179bbff2ee4a792e00989d77be465b4
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2020-05-02 19:37:28 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ let xhr= new XMLHttpRequest();
 xhr.onreadystatechange= function(){
     if(this.readyState===4 && this.status === 200) {
         console.log("working");
-        let data=xhr.responseText;
+        let data=xhr.response;
+        
         console.log(data);
     }
     else{
